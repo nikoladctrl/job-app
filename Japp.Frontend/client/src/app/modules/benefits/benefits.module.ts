2aabeece94faf51b619b3132da0f726e2c70797e
nikoladctrl <nikola.radanovic@digitalcontrol.me>
2022-03-29 09:17:57 +0200

    Frontend project's structure.

---
+++ Japp.Frontend/client/src/app/modules/benefits/benefits.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BenefitsRoutingModule } from './benefits-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BenefitsRoutingModule
+  ]
+})
+export class BenefitsModule { }
